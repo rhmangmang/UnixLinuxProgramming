f6a71303ae9f24c88d9ef974927653c9e82341d9
forJOS <1007181398@qq.com>
2018-05-29 05:18:21 -0700

    0.2version_of_more

--- more02.c
+++ more02.c
@@ -4,7 +4,7 @@
 #define LINELEN 512
 
 void do_more(FILE*);
-int see_more();
+int see_more(FILE*);
 
 int main(int ac, char* av[]) {
 	FILE* fp;
@@ -32,11 +32,16 @@ int main(int ac, char* av[]) {
 void do_more(FILE* fp) {
 	char line[LINELEN];
 	int num_of_lines = 0;
-//	int see_more(), reply;
-	int reply;
+	int see_more(FILE*), reply;
+
+	FILE* fp_tty;
+	fp_tty = fopen("/dev/tty", "r");
+	if (fp_tty == NULL) exit(1);
+
+
 	while (fgets(line, LINELEN, fp)) {
 		if (num_of_lines == PAGELEN) {
-			reply = see_more();
+			reply = see_more(fp_tty);
 			if (reply == 0) break;
 			num_of_lines -= reply;
 		}
@@ -45,10 +50,10 @@ void do_more(FILE* fp) {
 	}
 }
 
-int see_more() {
+int see_more(FILE* cmd) {
 	int c;
 	printf("\033[7m more?\033[m");
-	while ((c=getchar()) != EOF) {
+	while ((c=getc(cmd)) != EOF) {
 		if (c == 'q') return 0;
 		if (c == ' ') return PAGELEN;
 		if (c == '\n') return 1;
