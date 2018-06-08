33dcb858e39414604b77c093a656764253535dfe
rhmangmang <1007181398@qq.com>
2018-06-08 11:15:27 +0800

    0.1version_of_play_again

---
+++ playAgain/play_again1.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <termios.h>
+
+#define QUESTION "Do you want another transaction?"
+
+int get_response(char*);
+tty_mode(int);
+set_crmode();
+
+int main () {
+    int response;
+    tty_mode(0);
+    set_crmode();
+    response = get_response(QUESTION);
+    tty_mode(1);
+    return response;
+}
+
+int get_response (char* question) {
+    printf("%s(y/n)?", question);
+    while (1) {
+        switch (getchar()) {
+            case 'y':
+            case 'Y': return 0;
+            case 'n':
+            case 'N':
+            case EOF: return 1;
+        }
+    }
+}
+
+set_crmode() {
+    struct termios ttystate;
+    tcgetattr(0, &ttystate);
+    ttystate.c_lflag &= ~ICANON;
+    ttystate.c_cc[VMIN] = 1;
+    tcsetattr(0, TCSANOW, &ttystate);
+}
+
+tty_mode(int how) {
+    static struct termios original_mode;
+    if (how == 0) tcgetattr(0, &original_mode);
+    else return tcsetattr(0, TCSANOW, &original_mode);
+}
